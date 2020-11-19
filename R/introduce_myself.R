91974a60be7cceb66443e57f677ed48b14ca3ba2
ArthurData <arthur@critizr.com>
2020-11-19 01:51:55 +0100

    add introduce_myself

---
+++ R/introduce_myself.R
@@ -0,0 +1,10 @@
+#' @title introduce myself
+#'
+#'
+#' @return a string with an introduction about me
+#' @export
+#'
+#' @examples introduce_myself()
+introduce_myself <- function() {
+  "Hello, I'm pleased to introduce you to R"
+}
